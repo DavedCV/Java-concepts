4281fc86c1ed076fa5400da30c2514d919e39277
DavedCV <97773641+DavedCV@users.noreply.github.com>
2023-10-09 16:39:48 -0500

    Add files to ilustrate topics from collecting streams results

--- java-concepts/src/oop/inheritance/streams/CollectingResults.java
+++ java-concepts/src/oop/inheritance/streams/CollectingResults.java
@@ -1,4 +1,68 @@
 package oop.inheritance.streams;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class CollectingResults {
+    public static void main(String[] args) throws IOException {
+
+        // using iterator to collect results ---------------------------------------------------------------------------
+        Iterator<Integer> iter = Stream.iterate(0, n -> n + 1).limit(10).iterator();
+        while (iter.hasNext()) System.out.println(iter.next());
+
+        // using toArray to collect results ----------------------------------------------------------------------------
+        Object[] numbers1 = Stream.iterate(0, n -> n + 1).limit(10).toArray();
+        System.out.println("Object array:" + Arrays.toString(numbers1)); // Note it's an Object[] array
+
+        Integer[] numbers2 = Stream.iterate(0, n -> n + 1).limit(10).toArray(Integer[]::new);
+        System.out.println("Numbers array: " + Arrays.toString(numbers2));
+
+        // using collector to collect to a specified collection --------------------------------------------------------
+        Set<String> noVowelSet = noVowels().collect(Collectors.toSet());
+        show("noVowelSet", noVowelSet);
+
+        TreeSet<String> noVowelTreeSet = noVowels().collect(Collectors.toCollection(TreeSet::new));
+        show("noVowelTreeSet", noVowelTreeSet);
+
+        // using collector to concatenate strings ----------------------------------------------------------------------
+        String result = noVowels().limit(10).collect(Collectors.joining());
+        System.out.println("Joining: " + result);
+
+        String resultCommas = noVowels().limit(10).collect(Collectors.joining(", "));
+        System.out.println("Joining with commas: " + resultCommas);
+
+        // using summarizing -------------------------------------------------------------------------------------------
+        IntSummaryStatistics summary = noVowels().collect(Collectors.summarizingInt(String::length));
+
+        double averageWordLength = summary.getAverage();
+        double maxWordLength = summary.getMax();
+
+        System.out.println("Average word length: " + averageWordLength);
+        System.out.println("Max word length: " + maxWordLength);
+        System.out.println("For each: ");
+        noVowels().limit(10).forEach(System.out::println);
+    }
+
+    public static Stream<String> noVowels() throws IOException {
+        String contents = new String(Files.readAllBytes(Paths.get("src/oop/inheritance/streams/alice30.txt")),
+                                     StandardCharsets.UTF_8);
+        List<String> wordList = Arrays.asList(contents.split("\\PL+"));
+        Stream<String> words = wordList.stream();
+        return words.map(s -> s.replaceAll("[aeiouAEIOU]", ""));
+    }
+
+    public static <T> void show(String label, Set<T> set) {
+        System.out.println(label + ": " + set.getClass().getName());
+        System.out.println("[" + set.stream()
+                                    .limit(10)
+                                    .map(Object::toString)
+                                    .collect(Collectors.joining(", "))
+                                   + "]");
+    }
+
 }
---
+++ java-concepts/src/oop/inheritance/streams/DownstreamCollectors.java
@@ -0,0 +1,91 @@
+package oop.inheritance.streams;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class DownstreamCollectors {
+
+    public static Stream<City> readCities(String filename) throws IOException {
+        return Files.lines(Paths.get(filename)).map(l -> l.split(", "))
+                    .map(a -> new City(a[0], a[1], Integer.parseInt(a[2])));
+    }
+
+    public static void main(String[] args) throws IOException {
+        Stream<Locale> locales = Stream.of(Locale.getAvailableLocales());
+
+        // using downstream toSet collectors ---------------------------------------------------------------------------
+        Map<String, Set<Locale>> countryToLocaleSet = locales.collect(Collectors.groupingBy(Locale::getCountry,
+                                                                                            Collectors.toSet()));
+        System.out.println("countryToLocaleSet: " + countryToLocaleSet);
+
+        // using downstream counting collector -------------------------------------------------------------------------
+        locales = Stream.of(Locale.getAvailableLocales());
+        Map<String, Long> countryToLocaleCounts = locales.collect(Collectors.groupingBy(Locale::getCountry,
+                                                                                        Collectors.counting()));
+        System.out.println("countryToLocaleCounts: " + countryToLocaleCounts);
+
+        // using downstream summing collector --------------------------------------------------------------------------
+        Stream<City> cities = readCities("src/oop/inheritance/streams/cities.txt");
+        Map<String, Integer> stateToCityPopulation =
+                cities.collect(Collectors.groupingBy(City::getState, Collectors.summingInt(City::getPopulation)));
+        System.out.println("stateToCityPopulation: " + stateToCityPopulation);
+
+        // using downstream mapping and maxBy collector -----------------------------------------------
+        cities = readCities("src/oop/inheritance/streams/cities.txt");
+        Map<String, Optional<String>> stateToLongestCityName =
+                cities.collect(Collectors.groupingBy(
+                        City::getState,
+                        Collectors.mapping(City::getName, Collectors.maxBy(Comparator.comparing(String::length)))));
+        System.out.println("stateToLongestCityName: " + stateToLongestCityName);
+
+        // Using mapping and toSet downstream collectors -------------------------------------------------------------
+        locales = Stream.of(Locale.getAvailableLocales());
+        Map<String, Set<String>> countryToLanguages =
+                locales.collect(Collectors.groupingBy(Locale::getDisplayCountry,
+                                                      Collectors.mapping(Locale::getDisplayLanguage,
+                                                                         Collectors.toSet())));
+        System.out.println("countryToLanguages: " + countryToLanguages);
+
+        // using downstream summarizing collector ----------------------------------------------------------------------
+        cities = readCities("src/oop/inheritance/streams/cities.txt");
+        Map<String, IntSummaryStatistics> stateToCityPopulationSummary =
+                cities.collect(Collectors.groupingBy(City::getState, Collectors.summarizingInt(City::getPopulation)));
+        System.out.println(stateToCityPopulationSummary.get("NY"));
+
+        // using downstream reducing collector -------------------------------------------------------------------------
+        cities = readCities("src/oop/inheritance/streams/cities.txt");
+        Map<String, String> stateToCityNames =
+                cities.collect(Collectors.groupingBy(
+                        City::getState,
+                        Collectors.reducing("", City::getName, (s, t) -> s.isEmpty() ? t : s + ", " + t)));
+        System.out.println("stateToCityNames: " + stateToCityNames);
+    }
+
+    public static class City {
+        private String name;
+        private String state;
+        private int population;
+
+        public City(String name, String state, int population) {
+            this.name = name;
+            this.state = state;
+            this.population = population;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getState() {
+            return state;
+        }
+
+        public int getPopulation() {
+            return population;
+        }
+    }
+}
