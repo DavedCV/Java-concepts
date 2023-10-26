2b1347bdbf0289f65164960e92f3025679a5fe1d
DavedCV <97773641+DavedCV@users.noreply.github.com>
2023-10-26 14:16:59 -0500

    Spring: Using eager and lazy instantiation

--- spring/lazyInstantiation/src/main/java/org/example/Main.java
+++ spring/lazyInstantiation/src/main/java/org/example/Main.java
@@ -8,12 +8,11 @@ public class Main {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(ProjectConfiguration.class);
 
-        var cs1 = context.getBean("commentService", CommentService.class);
-        var cs2 = context.getBean("commentService", CommentService.class);
-
-        // The singleton bean scope always return the same instance
-        boolean b1 = cs1 == cs2;
-
-        System.out.println(b1);
+        System.out.println("Before retrieving the CommentService");
+        //  At this line, where Spring needs to provide a
+        //  reference to the CommentService bean,
+        //  Spring also creates the instance
+        var service = context.getBean(CommentService.class);
+        System.out.println("After retrieving the CommentService");
     }
 }
