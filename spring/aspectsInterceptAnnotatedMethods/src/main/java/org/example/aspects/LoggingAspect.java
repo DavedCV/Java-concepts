dd4dcf152f2cdd8d04d96f578cbf08ede5eb6b95
DavedCV <97773641+DavedCV@users.noreply.github.com>
2023-10-27 18:24:36 -0500

    Spring: Other advice annotations you can use - @AfterReturning example

--- spring/aspectsInterceptAnnotatedMethods/src/main/java/org/example/aspects/LoggingAspect.java
+++ spring/aspectsInterceptAnnotatedMethods/src/main/java/org/example/aspects/LoggingAspect.java
@@ -1,6 +1,7 @@
 package org.example.aspects;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.example.models.Comment;
@@ -13,6 +14,7 @@ public class LoggingAspect {
 
     private Logger logger = Logger.getLogger(LoggingAspect.class.getName());
 
+    /*
     @Around("@annotation(ToLog)") // Weaving the aspect to the methods annotated with @ToLog
     public void log(ProceedingJoinPoint joinPoint) throws Throwable {
 
@@ -25,5 +27,19 @@ public class LoggingAspect {
 
         // Delegates to the actual intercepted method
         joinPoint.proceed();
+    }
+    */
+
+    /*
+    * - The AspectJ pointcut expression specifies which methods this aspect logic weaves to
+    * - Optionally, when you use @AfterReturning, you can get the value returned by the intercepted method. In this
+    * case, we add the “returning” attribute with a value that corresponds to the name of the method’s parameter
+    * where this value will be provided.
+    * - The parameter name should be the same as the value of the “returning” attribute of the annotation or missing
+    * if we don’t need to use the returned value.
+    * */
+    @AfterReturning(value = "@annotation(ToLog)", returning = "returnedValue")
+    public void log(Object returnedValue) {
+        logger.info("Method executed and returned: " + returnedValue);
     }
 }
