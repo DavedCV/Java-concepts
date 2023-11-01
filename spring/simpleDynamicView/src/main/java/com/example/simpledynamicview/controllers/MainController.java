a32afec2dc08cdb1294d3bf92fe35d6b2b3c0af8
DavedCV <97773641+DavedCV@users.noreply.github.com>
2023-11-01 16:22:27 -0500

    Spring: use http path variables in the request

--- spring/simpleDynamicView/src/main/java/com/example/simpledynamicview/controllers/MainController.java
+++ spring/simpleDynamicView/src/main/java/com/example/simpledynamicview/controllers/MainController.java
@@ -2,21 +2,23 @@ package com.example.simpledynamicview.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
-
-    @RequestMapping("/home")
-    // We define a new parameter for the controller’s action method and annotate it with @RequestParam.
-    public String home(Model page, @RequestParam(required = false) String color) {
+    // To define a path variable, you assign it
+    // a name and put it in the path between
+    // curly braces.
+    @RequestMapping("/home/{color}")
+    // You mark the parameter where you want to get the path variable value
+    // with the @PathVariable annotation. The name of the parameter must be
+    // the same as the name of the variable in the path.
+    public String home(Model page, @PathVariable String color) {
         page.addAttribute("username", "David");
-
-        if (color != null) page.addAttribute("color", color);
-        else page.addAttribute("color", "blue");
-
+        page.addAttribute("color", color);
         return "home.html";
     }
 }
