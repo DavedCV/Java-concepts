f1e55a5cac5938ee39cf8ff6aa3ba2ced6b729af
DavedCV <97773641+DavedCV@users.noreply.github.com>
2023-11-08 23:08:46 -0500

    Spring: consuming REST services woth OpenFeign

---
+++ spring/consumingRestEndpoints-paymentService/src/main/java/com/example/consumingrestendpointspaymentservice/ConsumingRestEndpointsPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.consumingrestendpointspaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingRestEndpointsPaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumingRestEndpointsPaymentServiceApplication.class, args);
+    }
+
+}
