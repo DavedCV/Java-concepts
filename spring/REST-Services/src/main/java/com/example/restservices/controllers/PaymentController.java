c16d84897dddebf54be080c81aaf3bd1688141cd
DavedCV <97773641+DavedCV@users.noreply.github.com>
2023-11-08 21:29:54 -0500

    Spring: Using a request body to get data from the client

--- spring/REST-Services/src/main/java/com/example/restservices/controllers/PaymentController.java
+++ spring/REST-Services/src/main/java/com/example/restservices/controllers/PaymentController.java
@@ -6,11 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
 
 @RestController
 public class PaymentController {
     private final PaymentService paymentService;
+    private static Logger logger = Logger.getLogger(PaymentController.class.getName());
 
     @Autowired
     public PaymentController(PaymentService paymentService) {
@@ -18,8 +22,17 @@ public class PaymentController {
     }
 
     @PostMapping("/payment")
-    public ResponseEntity<?> makePayment() {
-        PaymentDetails paymentDetails = paymentService.processPayment();
+    // We get the payment details from the HTTP request body.
+    public ResponseEntity<PaymentDetails> makePayment(@RequestBody PaymentDetails paymentDetails) {
+
+        // We log the amount of the payment in the server’s console.
+        logger.info("Received payment: " + paymentDetails.getAmount());
+
+        // PaymentDetails paymentDetails = paymentService.processPayment();
+
+        // We send back the payment details object
+        // in the HTTP response body, and we set the
+        // HTTP response status to 202 ACCEPTED
         return ResponseEntity
                 .status(HttpStatus.ACCEPTED)
                 .body(paymentDetails);
