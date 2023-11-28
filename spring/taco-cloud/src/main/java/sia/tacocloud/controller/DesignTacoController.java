93180ba72d9068d8034b899b0b8dddbdf9762320
DavedCV <97773641+DavedCV@users.noreply.github.com>
2023-11-28 17:49:41 -0500

    Spring: add validation to controllers requests

--- spring/taco-cloud/src/main/java/sia/tacocloud/controller/DesignTacoController.java
+++ spring/taco-cloud/src/main/java/sia/tacocloud/controller/DesignTacoController.java
@@ -1,8 +1,10 @@
 package sia.tacocloud.controller;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import sia.tacocloud.domain.Ingredient;
 import sia.tacocloud.domain.Ingredient.Type;
@@ -62,7 +64,10 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder) {
+    public String processTaco(@Valid Taco taco, Errors errors, @ModelAttribute TacoOrder tacoOrder) {
+
+        if (errors.hasErrors()) return "design";
+
         tacoOrder.addTaco(taco);
         log.info("Processing taco: {}", taco);
 
