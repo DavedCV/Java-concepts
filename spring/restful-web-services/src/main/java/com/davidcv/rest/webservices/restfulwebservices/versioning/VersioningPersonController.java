94c88dcecc64f045233e94746c8b4aa20200dd3d
DavedCV <97773641+DavedCV@users.noreply.github.com>
2024-02-03 16:53:12 -0500

    Add sample choices for api versioning

--- spring/restful-web-services/src/main/java/com/davidcv/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
+++ spring/restful-web-services/src/main/java/com/davidcv/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VersioningPersonController {
+
+    // Using URL -------------------------------------------------------------------------------------------------------
+
     @GetMapping("/v1/person")
     public PersonV1 getFirstVersionOfPerson() {
         return new PersonV1("David Castrillón");
@@ -14,4 +17,30 @@ public class VersioningPersonController {
     public PersonV2 getSecondVersionOfPerson() {
         return new PersonV2("David", "Castrillón");
     }
+
+    // Using Request Param ---------------------------------------------------------------------------------------------
+
+    @GetMapping(path = "/person", params = "version=1")
+    public PersonV1 getFirstVersionOfPersonRequestParam() {
+        return new PersonV1("David Castrillón");
+    }
+
+
+    @GetMapping(path = "/person", params = "version=2")
+    public PersonV2 getSecondVersionOfPersonRequestParam() {
+        return new PersonV2("David", "Castrillón");
+    }
+
+    // Using headers ---------------------------------------------------------------------------------------------------
+
+    @GetMapping(path = "/person", headers = "X-API-VERSION=1")
+    public PersonV1 getFirstVersionOfPersonHeader() {
+        return new PersonV1("David Castrillón");
+    }
+
+    @GetMapping(path = "/person", headers = "X-API-VERSION=2")
+    public PersonV2 getSecondVersionOfPersonHeader() {
+        return new PersonV2("David", "Castrillón");
+    }
+
 }
