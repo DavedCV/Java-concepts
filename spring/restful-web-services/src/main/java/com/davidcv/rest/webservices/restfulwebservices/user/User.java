8ae2c99bfd6a235e76c226810598c99665d164b5
DavedCV <97773641+DavedCV@users.noreply.github.com>
2024-02-05 14:04:23 -0500

    Create examples to manipulate the json response from controller methods: - change names of field properties - static filtering of some properties

--- spring/restful-web-services/src/main/java/com/davidcv/rest/webservices/restfulwebservices/user/User.java
+++ spring/restful-web-services/src/main/java/com/davidcv/rest/webservices/restfulwebservices/user/User.java
@@ -1,5 +1,6 @@
 package com.davidcv.rest.webservices.restfulwebservices.user;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -8,8 +9,10 @@ import java.time.LocalDate;
 public class User {
     private long id;
     @Size(min = 2, message = "Name should have at least 2 characters")
+    @JsonProperty(value = "user_name")
     private String name;
     @Past(message = "Birthday should be in the past")
+    @JsonProperty(value = "birth_date") // change the name of the property json name
     private LocalDate birthDate;
 
     public User() {
